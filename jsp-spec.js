27c159e206c4bae4e77e0f1862358a22f8513e2d
Kirill Levshin <klevshin@mail.ru>
2016-02-16 11:18:20 +0300

    'js5'

--- jsp-spec.js
+++ jsp-spec.js
@@ -1,141 +1,42 @@
 
-function AngularToDoPage() {
-  var input = element(by.model('todoList.todoText'));
-  var addButton = element(by.buttonText('add'));
-  var todoListItems = element.all(by.repeater('todo in todoList.todos'));
-  var todoListCheckboxes = element.all(by.model('todo.done'));
-  var remainingField = element(by.binding('todoList.remaining()'));
-  var archiveButton = element(by.linkText('archive'));
- 
-  this.enterText = function(text) {
-    input.sendKeys(text);
-  };
 
-  this.clickAddButton = function() {
-    addButton.click();
-  };
-
-  this.todoListGetCount = function () {
-      return todoListItems.count();
-  }
-
-  this.todoListGetItem = function (index) {
-      return todoListItems.get(index).getText();
-  }
-
-  this.todoListClickChecbox = function (index) {
-      todoListCheckboxes.get(index).click();
-  }
-
-  this.getRemainingField = function () {
-      return remainingField.getText();
-  }
-
-  this.clickArchiveButton = function () {
-      archiveButton.click();
-  }
-
-}
-
-
-
-function ToDoItem() {
-    var angulartodopage = new AngularToDoPage();
-
-    this.add = function (name) {
-        angulartodopage.enterText(name);
-        angulartodopage.clickAddButton();
-        
-    }
-
-    this.getCount = function () {
-        return angulartodopage.todoListGetCount();
-    }
-
-    this.getName = function (index) {
-        return angulartodopage.todoListGetItem(index);
-    }
- 
-    this.getRemainingFieldValue = function () {
-        return angulartodopage.getRemainingField();
-    }
-
-    this.markDone = function (index) {
-        angulartodopage.todoListClickChecbox(index);
-    }
-
-    this.archive = function () {
-        angulartodopage.clickArchiveButton();
-    }
-
-}
-
-
-var defaultItem1Name = 'learn angular';
-var defaultItem2Name = 'build an angular app';
-var inputItemName1 = 'item1';
-var inputItemName2 = 'Artifact';
-var inputItemName3 = 'item%^&';
-var inputItemName4 = 'item >12';
-var inputItemName5 = '88item';
 
 beforeEach(function () {
     browser.get('http://www.angularjs.org');
 });
 
 
-describe('Angular homepage', function() {
+describe('Wire up a Backend', function () {
 
-    it('Adding 1 new item', function () {
+    it('Wire up a Backend', function () {
 
-        var todoitem = new ToDoItem();
+        browser.sleep(5000);
 
-        todoitem.add(inputItemName1);
+        var projects = element.all(by.repeater('project in projectList.projects'));
+        var seacrhField = element(by.model('projectList.search'));
+        //var addProjectButton = element(by.className('icon-plus-sign'));
+        editProjectButton = element.all(by.className('icon-pencil'));
 
-        expect(todoitem.getCount()).toEqual(3);
-        expect(todoitem.getName(0)).toEqual(defaultItem1Name);
-        expect(todoitem.getName(1)).toEqual(defaultItem2Name);
-        expect(todoitem.getName(2)).toEqual(inputItemName1);
-        expect(todoitem.getRemainingFieldValue()).toEqual('2 of 3 remaining');
+        editProjectButton.get(5).click();
+
+        browser.sleep(5000);
+        /*
+        var a = element(by.buttonText('Save'));
+        var a = element(by.buttonText('Delete'));
+        */
+
+        var a = element(by.model('editProject.project.description'));
+
+        
+
+        
+
+        expect(a.getText()).toEqual(true);
+     
 
     })
 
-    it('Adding 2 new items', function () {
-        
-        var todoitem = new ToDoItem();
-
-        todoitem.add(inputItemName4);
-        todoitem.add(inputItemName5);
-        
-        expect(todoitem.getCount()).toEqual(4);
-        expect(todoitem.getName(0)).toEqual(defaultItem1Name);
-        expect(todoitem.getName(1)).toEqual(defaultItem2Name);
-        expect(todoitem.getName(2)).toEqual(inputItemName4);
-        expect(todoitem.getName(3)).toEqual(inputItemName5);
-        expect(todoitem.getRemainingFieldValue()).toEqual('3 of 4 remaining');
-         
-    })
-    
-
-    it('Archiving several items', function () {
-        
-        var todoitem = new ToDoItem();
-
-        todoitem.add(inputItemName2);
-        todoitem.add(inputItemName3);
-
-        todoitem.markDone(2);
-
-        todoitem.archive();
-
-        expect(todoitem.getCount()).toEqual(2);
-        expect(todoitem.getName(0)).toEqual(defaultItem2Name);
-
-        expect(todoitem.getName(1)).toEqual(inputItemName3);
-
-        expect(todoitem.getRemainingFieldValue()).toEqual('2 of 2 remaining');
-
-    })
+   
 
     });
 
