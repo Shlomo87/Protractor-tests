d476e6e9cf5514998d4220a123b2c4f1403adc2d
Kirill Levshin <klevshin@mail.ru>
2016-02-16 18:17:52 +0300

    'pt3'

--- jsp-spec.js
+++ jsp-spec.js
@@ -93,98 +93,16 @@ beforeEach(function () {
 
 describe('Wire up a Backend', function () {
 
-    it('Search by project name', function () {
+    it('Check projects list', function () {
 
         var project = new Project();
 
-        project.add(projectName, projectWebsite, projectDescription);
+        expect(project.getCount()).toEqual(12);
+        expect(project.getNameDesciption(4)).toEqual('Ember Ambitious web apps.');
+        expect(project.getNameDesciption(9)).toEqual('React A JavaScript library for building user interfaces.');
 
-        browser.sleep(3000);
-        
-        expect(project.getCount()).toEqual(13);
-        expect(project.getNameDesciption(9)).toEqual(projectName + ' ' + projectDescription);
+    })
 
-    })   
-    
-
-
-
-
-
-
-
-    
-    
-    
-    
-    
-
-
-        /*
-        var projects = element.all(by.repeater('project in projectList.projects'));
-        var seacrhField = element(by.model('projectList.search'));
-        //var addProjectButton = element(by.className('icon-plus-sign'));
-        editProjectButton = element.all(by.className('icon-pencil'));
-
-        editProjectButton.get(5).click();
-
-        browser.sleep(5000);
-    
-        var a = element(by.buttonText('Save'));
-        var a = element(by.buttonText('Delete'));
-    
-    var a = element(by.model('editProject.project.description'));
-        */
-
-        // Edit - Name, Website, Description fields
-        /*
-        var editProject = element.all(by.className('icon-pencil'));
-
-        editProject.get(6).click();
-
-        browser.sleep(2000);
-
-        var inputWebsite = element(by.model('editProject.project.site'));
-        inputWebsite.sendKeys('aaaa');
-        browser.sleep(8000);
-        expect(inputWebsite.getAttribute('value')).toEqual(true);
-       
-        var inputName = element(by.model('editProject.project.name'));
-        inputName.sendKeys('aaaa');
-        browser.sleep(8000);
-        expect(inputName.getAttribute('value')).toEqual(true);
-        
-        var inputDescription = element(by.model('editProject.project.description'));
-        inputDescription.sendKeys('aaaa');
-        browser.sleep(8000);
-        expect(inputDescription.getAttribute('value')).toEqual(true);
-       
-
-        var link = element(by.linkText("Cappucino"));
-
-        link.click();
-
-        
-        browser.getAllWindowHandles().then(function (handles) {
-            var secondWindowHandle = handles[1];
-            var firstWindowHandle = handles[0];
-
-            browser.switchTo().window(firstWindowHandle);
-            browser.sleep(5000);
-            browser.switchTo().window(secondWindowHandle);
-        });
-                          
-
-
-        browser.sleep(5000);
-
-        expect(browser.driver.getCurrentUrl()).toEqual(true);
-
-        */
-
-
-
-    /*
     it('Search by project name', function () {
 
         var project = new Project();
@@ -207,22 +125,23 @@ describe('Wire up a Backend', function () {
 
     })
 
-    it('Check projects list', function () {
+    it('Adding project', function () {
 
         var project = new Project();
+
+        project.add(projectName, projectWebsite, projectDescription);
+
+        browser.sleep(3000);
         
-        expect(project.getCount()).toEqual(12);
-        expect(project.getNameDesciption(4)).toEqual('Ember Ambitious web apps.');
-        expect(project.getNameDesciption(9)).toEqual('React A JavaScript library for building user interfaces.');
- 
-    })
-    */
+        expect(project.getCount()).toEqual(13);
+        expect(project.getNameDesciption(9)).toEqual(projectName + ' ' + projectDescription);
 
-
-
+    })   
+    
     
 
-
+    
+     
     });
 
 
