d40ba8a2957f8af1c65839b0a4ba6d6455a3758d
Kirill Levshin <klevshin@mail.ru>
2016-02-17 11:56:30 +0300

    'jsp10'

--- jsp-spec.js
+++ jsp-spec.js
@@ -8,6 +8,7 @@ function WireUpBackendPage() {
     var inputWebsite = element(by.model('editProject.project.site'));
     var inputDescription = element(by.model('editProject.project.description'));
     var saveButton = element(by.buttonText('Save'));
+    this.linkCappucino = element(by.linkText("Cappucino"));
 
     this.getProjectsCount = function () {
         return projects.count();
@@ -73,6 +74,12 @@ function Project() {
         wireupbackendpage.clickSaveButton();
     }
 
+    this.clickCappucinoLink = function () {
+        wireupbackendpage.linkCappucino.click();
+    }
+
+
+
             
 
 
@@ -87,7 +94,9 @@ var projectDescription = 'desciption';
 
 beforeEach(function () {
     browser.get('http://www.angularjs.org');
-    browser.sleep(5000);
+    browser.wait(function () {
+        return element(by.linkText("Cappucino")).isPresent();
+    }, 10000);
 });
 
 
@@ -136,9 +145,23 @@ describe('Wire up a Backend', function () {
         expect(project.getCount()).toEqual(13);
         expect(project.getNameDesciption(9)).toEqual(projectName + ' ' + projectDescription);
 
-    })   
-    
-    
+    })
+
+    it('Go to project site', function () {
+
+        var project = new Project();
+
+        project.clickCappucinoLink();
+
+        browser.sleep(3000);
+
+        browser.getAllWindowHandles().then(function (windowHandles) {
+            browser.switchTo().window(windowHandles[1]);
+        });
+
+        expect(browser.driver.getCurrentUrl()).toEqual('http://www.cappuccino-project.org/');
+
+    })
 
     
      
